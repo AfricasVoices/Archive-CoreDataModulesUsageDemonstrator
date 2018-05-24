426ac54ae6172039056fb3b273e8204306f8514a
Alexander Simpson <as2388@cam.ac.uk>
2018-05-24 11:51:53 +0100

    Update CoreData dependency and pass user into import_coda

--- merge_coda_coded/merge_coda_coded.py
+++ merge_coda_coded/merge_coda_coded.py
@@ -7,11 +7,11 @@ from core_data_modules.traced_data.io import TracedDataCodaIO
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Applies Coda-coded data to TracedData JSON file")
     parser.add_argument("user", help="User launching this program", nargs=1)
-    parser.add_argument("raw_column", metavar="raw-column", help="Name of raw input column to apply code to", nargs=1)
-    parser.add_argument("coded_column", metavar="coded-column", help="Name of column to store code in", nargs=1)
+    parser.add_argument("raw_column", metavar="raw-column", help="Name of raw message column to apply code to", nargs=1)
+    parser.add_argument("coded_column", metavar="coded-column", help="Name of column to store code to", nargs=1)
     parser.add_argument("input", help="Path to input file, containing a list of TracedData objects as JSON", nargs=1)
-    parser.add_argument("coda_input", metavar="coda-input", help="TODO", nargs=1)
-    parser.add_argument("output", help="Path to write results of automatic coding to", nargs=1)
+    parser.add_argument("coda_input", metavar="coda-input", help="Path to a coded Coda data file", nargs=1)
+    parser.add_argument("output", help="Path to write merged dataset to, as JSON", nargs=1)
 
     args = parser.parse_args()
     user = args.user[0]
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     coda_input_path = args.coda_input[0]
     output_path = args.output[0]
 
-    col = args.raw_column[0]
+    col_raw = args.raw_column[0]
     col_coded = args.coded_column[0]
 
     # Load data
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 
     # Read Coda input
     with open(coda_input_path, "rb") as f:
-        data = list(TracedDataCodaIO.import_coda_to_traced_data_iterable(data, col, col_coded, f))
+        data = list(TracedDataCodaIO.import_coda_to_traced_data_iterable(user, data, col_raw, col_coded, f))
 
     # Write new output
     with open(output_path, "w") as f:
